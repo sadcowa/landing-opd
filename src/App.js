3a894faf9c073b2d916f95e7f692a2ea24b9f904
Anna <annasadkova20479932@gmail.com>
2024-10-17 16:28:21 +0700

    Тестовый хедер

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+function Header(){
+  return(
+    <header className="App-header">
+      <div className="button-container">
+        <button className="btn">О курсе</button>
+        <button className="btn">Наши проекты</button>
+        <button className="btn">Наставники</button>
+        <button className="btn feedback-btn">Обратная связь</button>
+      </div>
+    </header>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      
+        <Header />
     </div>
   );
 }
