a0a04fb0b710b44191f058ce12be332ff8047334
Anna <annasadkova20479932@gmail.com>
2024-12-09 21:03:16 +0700

    валидация

--- src/pages/FeedbackForm/FeedbackForm.js
+++ src/pages/FeedbackForm/FeedbackForm.js
@@ -1,112 +1,173 @@
 import React, { useState } from 'react';
 import './FeedbackForm.css';
 import Input from '../../components/UI-kit/Input/Input';
-import TextArea from '../../components/UI-kit/TextArea/TextArea';
+import TextArea from '../../components/UI-kit/Textarea/Textarea';
 import SubmitButton from '../../components/UI-kit/SubmitButton/SubmitButton';
 
 const FeedbackForm = () => {
-  const [fio, setFio] = useState("Иванов Иван Иванович");
-  const [group, setGroup] = useState("123-4");
-  const [email, setEmail] = useState("Ivanov_Ivan@mail.ru");
-  const [message, setMessage] = useState("Введите сообщение...");
-  
-  const [fioColor, setFioColor] = useState("#a8a8a8");
-  const [groupColor, setGroupColor] = useState("#a8a8a8");
-  const [emailColor, setEmailColor] = useState("#a8a8a8");
-  const [messageColor, setMessageColor] = useState("#a8a8a8");
+  const [fio, setFio] = useState("");
+  const [group, setGroup] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [isError, setIsError] = useState(false); 
+  const [isEmailError, setIsEmailError] = useState(false); 
+  const [isFioError, setIsFioError] = useState(false); 
+  const [isGroupError, setIsGroupError] = useState(false); 
+  const [isDataSent, setIsDataSent] = useState(false);
+  const [isDataSending, setIsDataSending] = useState(false);
 
-  const handleFocus = (setter) => {
-    setter(""); 
-  };
+  const onHandlerSend = (e) => {
+    e.preventDefault();
+    setIsDataSending(true);
 
-  const handleBlur = (value, setter, defaultValue, colorSetter, defaultColor) => {
-    if (!value) {
-      setter(defaultValue);
-      colorSetter(defaultColor);
+    if (!fio || !group || !email || !message) {
+      setIsError(true);
+      setIsDataSending(false);
+      return;
     }
-  };
+
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(email)) {
+      setIsEmailError(true);
+      setIsDataSending(false);
+      return;
+    }
+
+    const fioRe = /^[А-ЯЁ][а-яё]+\s[А-ЯЁ][а-яё]+(\s[А-ЯЁ][а-яё]+)?$/;
+    if (!fioRe.test(fio)) {
+      setIsFioError(true);
+      setIsDataSending(false);
+      return;
+    }
+
+    const groupRe = /^[0-9]+(-[А-ЯЁ0-9]+)?$/;
+    if (!groupRe.test(group)) {
+      setIsGroupError(true);
+      setIsDataSending(false);
+      return;
+    }
+
+    fetch('http://localhost:5000/mail', {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        fio: fio,
+        group: group,
+        email: email,
+        message: message
+      })
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(() => {
+        setIsDataSent(true);
+        setIsDataSending(false);
+      })
+      .catch((er) => {
+        console.error('Error sending data: ', er);
+        setIsDataSending(false);
+      });
+  }
+
+  const isFormValid = () => {
+    return !isError && !isEmailError && !isFioError && !isGroupError && !isDataSending;
+  }
+
+  const handleInputChange = (setter, value) => {
+    setter(value);
+    if (isError) setIsError(false);
+    if (isEmailError) setIsEmailError(false);
+    if (isFioError) setIsFioError(false);
+    if (isGroupError) setIsGroupError(false);
+    if (isDataSent) setIsDataSent(false);
+  }
 
   return (
-    <form id="feedback-section" className="feedback-form">
-      <h1>
-        <span className="text-green">Обратная</span> <span className="text-white">связь</span>
-      </h1>
-      <div className="form-group">
-        <label htmlFor="fio">ФИО:</label>
-        <Input 
-          id="fio" 
-          value={fio} 
-          style={{ color: fioColor, width: '1340px', height: '50px', marginBottom: '40px' }} 
-          onFocus={() => {
-            handleFocus(setFio);
-            setFioColor("#000000"); 
-          }} 
-          onBlur={() => {
-            handleBlur(fio, setFio, "Иванов Иван Иванович", setFioColor, "#a8a8a8");
-          }} 
-          onChange={(e) => setFio(e.target.value)} 
-        />
-      </div>
-
-      <div className="form-flex">
-        <div className="form-flex-column">
-          <div className="form-group">
-            <label htmlFor="group">Группа обучения:</label>
-            <Input 
-              id="group" 
-              value={group} 
-              style={{ color: groupColor, width: '650px', height: '48px' }} 
-              onFocus={() => {
-                handleFocus(setGroup);
-                setGroupColor("#000000"); 
-              }} 
-              onBlur={() => {
-                handleBlur(group, setGroup, "123-4", setGroupColor, "#a8a8a8");
-              }} 
-              onChange={(e) => setGroup(e.target.value)} 
-            />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <Input 
-              id="email" 
-              value={email} 
-              style={{ color: emailColor, width: '650px', height: '48px' }} 
-              onFocus={() => {
-                handleFocus(setEmail);
-                setEmailColor("#000000"); 
-              }} 
-              onBlur={() => {
-                handleBlur(email, setEmail, "Ivanov_Ivan@mail.ru", setEmailColor, "#a8a8a8");
-              }} 
-              onChange={(e) => setEmail(e.target.value)} 
-            />
-          </div>
-        </div>
-
+    <>
+      <form id="feedback-section" className="feedback-form">
+        <h1>
+          <span className="text-green">Обратная</span> <span className="text-white">связь</span>
+        </h1>
         <div className="form-group">
-          <label htmlFor="message">Сообщение:</label>
-          <TextArea 
-            id="message" 
-            value={message} 
-            style={{ color: messageColor, width: '650px', height: '190px' }} 
-            onFocus={() => {
-              handleFocus(setMessage);
-              setMessageColor("#000000"); 
-            }} 
-            onBlur={() => {
-              handleBlur(message, setMessage, "Введите сообщение...", setMessageColor, "#a8a8a8");
-            }} 
-            onChange={(e) => setMessage(e.target.value)} 
+          <label htmlFor="fio">ФИО:</label>
+          <Input
+            id="fio"
+            placeholder="Иванов Иван Иванович"
+            value={fio}
+            style={{ width: '1340px', height: '50px', marginBottom: '40px' }}
+            onChange={(e) => handleInputChange(setFio, e.target.value)}
+            onFocus={(e) => e.target.placeholder = ""}
+            onBlur={(e) => e.target.placeholder = "Иванов Иван Иванович"}
           />
         </div>
-      </div>
 
-      <div className="button-container">
-        <SubmitButton type="submit">Отправить</SubmitButton>
-      </div>
-    </form>
+        <div className="form-flex">
+          <div className="form-flex-column">
+            <div className="form-group">
+              <label htmlFor="group">Группа обучения:</label>
+              <Input
+                id="group"
+                value={group}
+                placeholder="123-4"
+                style={{ width: '650px', height: '48px' }}
+                onChange={(e) => handleInputChange(setGroup, e.target.value)}
+                onFocus={(e) => e.target.placeholder = ""}
+                onBlur={(e) => e.target.placeholder = "123-4"}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <Input
+                id="email"
+                type="email"
+                isEmailError={isEmailError}
+                placeholder="Ivanov_Ivan@mail.ru"
+                value={email}
+                style={{ width: '650px', height: '48px' }}
+                onChange={(e) => handleInputChange(setEmail, e.target.value)}
+                onFocus={(e) => e.target.placeholder = ""}
+                onBlur={(e) => e.target.placeholder = "Ivanov_Ivan@mail.ru"}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="message">Сообщение:</label>
+            <TextArea
+              id="message"
+              placeholder="Введите сообщение..."
+              value={message}
+              style={{ width: '650px', height: '190px' }}
+              onChange={(e) => handleInputChange(setMessage, e.target.value)}
+              onFocus={(e) => e.target.placeholder = ""}
+              onBlur={(e) => e.target.placeholder = "Введите сообщение..."}
+            />
+          </div>
+        </div>
+
+        {(isError || isEmailError || isFioError || isGroupError || isDataSent) && (
+          <div className={`message-text ${isDataSent ? 'success' : ''}`}>
+          {isError ? 'Пожалуйста, заполните все поля' : 
+           isEmailError ? 'Пожалуйста, введите корректный адрес почты' : 
+           isFioError ? 'Пожалуйста, введите корректное ФИО' : 
+           isGroupError ? 'Пожалуйста, введите корректный номер группы' : 
+           'Спасибо за обращение! Вскоре мы Вам ответим'}
+        </div>
+        )}
+
+        <div className="button-container">
+          <SubmitButton disabled={!isFormValid()} onClick={(e) => onHandlerSend(e)}>Отправить</SubmitButton>
+        </div>
+      </form>
+    </>
   );
 };
 
